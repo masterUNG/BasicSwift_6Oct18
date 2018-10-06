62a63530f99d3c3df5c17a29327bf18b46e6c090
masterUNG <phrombutr@gmail.com>
2018-10-06 14:29:52 +0700

    Understant let

--- VariableAndConstant.playground/Contents.swift
+++ VariableAndConstant.playground/Contents.swift
@@ -8,6 +8,26 @@ var numberInt = 123             // Int Type
 var numberDou = 12.34           // Double Type
 var statusBool = true           // Boolene Type
 
+//Try to Change Value
+//var สามารถแก้ไขค่าได้
+nameString = "Doramon"
+numberInt = 555
+numberDou = 3.14
+statusBool = !statusBool
+
+//This is Constant
+let surNameString = "Phrombutr"
+//surNameString = "AAAAA"   ไม่สามารถแก้ไขค่าได้ เพราะเป็น let
+
+let number1Int = 333
+let number1Dou = 12.345
+let status1Bool = false
+
+
+
+
+
+
 
 
 
