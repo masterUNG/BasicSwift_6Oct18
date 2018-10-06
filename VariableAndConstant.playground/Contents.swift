38d5e6b1148408086d343459f500521f46fa87a8
masterUNG <phrombutr@gmail.com>
2018-10-06 14:19:21 +0700

    Var

---
+++ VariableAndConstant.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//การประกาศตัวแปร
+
+//นี่คือการประกาศตัวแปร แบบ Implicit หรือ การประกาศแบบให้ Value ในการกำหนด Data Type
+var nameString = "Master UNG"   // String Type
+var numberInt = 123             // Int Type
+var numberDou = 12.34           // Double Type
+var statusBool = true           // Boolene Type
+
+
+
+
