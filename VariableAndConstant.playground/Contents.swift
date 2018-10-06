34e9e5f2dd08a84eca09b4371add97d23c7f174e
masterUNG <phrombutr@gmail.com>
2018-10-06 14:44:21 +0700

    Operating

--- VariableAndConstant.playground/Contents.swift
+++ VariableAndConstant.playground/Contents.swift
@@ -8,6 +8,13 @@ var numberInt = 123             // Int Type
 var numberDou = 12.34           // Double Type
 var statusBool = true           // Boolene Type
 
+//Explicit
+var number2String: String = "Nopita"
+var number2Int: Int = 123
+var number2Dou: Double = 22.44
+var status2Bool: Bool = true
+
+
 //Try to Change Value
 //var สามารถแก้ไขค่าได้
 nameString = "Doramon"
@@ -22,6 +29,19 @@ let surNameString = "Phrombutr"
 let number1Int = 333
 let number1Dou = 12.345
 let status1Bool = false
+
+//Explicit
+let number3Int: Int = 55
+
+//Operateting
+let answer1Int: Int = numberInt + Int(number1Dou)    // Change Double to Int
+let answer1Dou: Double = numberDou + Double(number1Int) // Int to Double
+let answer1String: String = nameString + surNameString + String(number1Int)
+
+
+
+
+
 
 
 
